4b09a80bdbac3110368a8ea73be9f168c574f2af
Fareno-237 <giffareno237@gmail.com>
2025-06-04 12:06:52 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:fareno12@localhost/fareno_university')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('fareno237')
